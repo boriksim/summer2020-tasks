f75ce57b730ab279f17da48a12a65a454b3274df
Borislav <borik.sim@gmail.com>
2020-06-07 22:00:34 +0300

    task8 - read create txt files

---
+++ task8-read_create_txt_files/read_create_txt_files.py
@@ -0,0 +1,22 @@
+import sys
+
+try:
+    file1 = open(sys.argv[1], "r")
+    file2 = open(sys.argv[2], "w+")
+except IndexError:
+    print("please input 2 file names")
+    sys.exit()
+except FileNotFoundError as e:
+    print("file \"{}\" not found".format(e.filename))
+    sys.exit()
+try:
+    for line in file1.readlines():
+        try:
+            file2.write("{}\n".format(max(list(map(int, line.split())))))
+        except ValueError:
+            print("file contains non-integers")
+            sys.exit()
+except UnicodeDecodeError:
+    print("something went wrong")
+file1.close()
+file2.close()

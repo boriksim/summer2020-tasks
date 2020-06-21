6c516180395291e32b566766ddc32dfc2192e473
Borislav <borik.sim@gmail.com>
2020-06-21 19:54:09 +0300

    task19 - Vigenere cipher (fix)

--- task019-Vigenere-cipher/Vigenere-cipher.py
+++ task019-Vigenere-cipher/Vigenere-cipher.py
@@ -1,23 +1,46 @@
 # 2020-06-19
 # Шифр Виженера
+import argparse
+import os.path
 
+parser = argparse.ArgumentParser(description='Process some integers.')
+parser.add_argument("srcfile", metavar="src", type=str, help="source filename")
+parser.add_argument("dstfile", metavar="dst", type=str, help="destination filename")
+parser.add_argument("--pwd", metavar="password", type=str, help="password", required=True)
+parser.add_argument("--force", default=False, action="store_true", help="please mention to have existing file overwritten")
+args = parser.parse_args()
+pwd = args.pwd
 
-def Vigenere_cipher(input, password):
-    input_codes = [ord(i) for i in input]
-    password_codes = [ord(i) for i in password]
-    output_chars = []
+bufsize = 1024
 
-    for b in range(len(input_codes)):
-        output_chars.append(input_codes[b] ^ password_codes[b % len(password_codes)])
+if os.path.exists(args.srcfile):
+    src = open(args.srcfile, "rb")
+else:
+    print("srcfile not found")
+    exit()
 
-    for i in range(len(output_chars)):
-        output_chars[i] = chr(output_chars[i])
+if args.force or not os.path.exists(args.dstfile):
+    dst = open(args.dstfile, "wb")
+else:
+    print("dstfile already exists. Use \"--force\" to overwrite it")
+    exit()
 
-    output = "".join(output_chars)
+pos = 0
+while True:
+    block = src.read(bufsize)
+    if not block:
+        break
+    block2 = [ord('-')]*len(block)
+    i = 0
+    for b in block:
+        pwd_char = pwd[pos % len(pwd)]
+        dst_char = b ^ ord(pwd_char)
+        # block2.append(dst_char)
+        block2[i] = dst_char
+        i += 1
+        pos += 1
 
-    return output
+    dst.write(bytes(block2))
 
-
-a = Vigenere_cipher("Tree. Apple, ball", "asd")
-a = Vigenere_cipher(a, "asd")
-print(a)
+src.close()
+dst.close()
